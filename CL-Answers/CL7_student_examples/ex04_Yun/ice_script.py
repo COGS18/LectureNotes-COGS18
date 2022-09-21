2f459308871bf2582dff441d86f0f1d0f9546c9d
Jason Fleischer <jason.g.fleischer@gmail.com>
2022-09-21 11:04:14 -0700

    cleaned up ready for Fall 22

--- CL-Answers/CL7_student_examples/ex04_Yun/ice_script.py
+++
@@ -1,5 +0,0 @@
-import ice
-chocolate=ice.Icecream(size='small')
-print(ice.my_fav(chocolate))
-
-print(ice.my_size(chocolate))
--- CL-Answers/CL7_student_examples/ex06_Ashesh/script.py
+++
@@ -1,4 +0,0 @@
-import diary_module as dm
-new_diary = dm.Diary()
-new_diary.diary_entry('first entry')
-print(new_diary.entries)
