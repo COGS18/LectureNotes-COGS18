6e6a96a008564373e6a987a024da77707fbb5e1a
Brian Hempel <git@brianhempel.com>
2024-01-08 21:52:04 -0800

    reset for wi24

--- CodeTesting/test_my_functions.py
+++
@@ -1,9 +0,0 @@
-def test_divide_neighbors():
-    assert type(divide_neighbors([1, 2])) == list
-    assert divide_neighbors([1, 2, 4]) == [2, 2]
-
-
-def test_sum_list():
-    assert callable(sum_list)
-    assert type(sum_list([1, 2, 3, 4])) == int
-    assert sum_list([1, 2, 3, 4]) == 10
