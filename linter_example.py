682ff271c6283b20960b3b9061c554ff3cd4776e
Brian Hempel <git@brianhempel.com>
2024-03-04 15:35:18 -0800

    Documentation Lecture

---
+++ linter_example.py
@@ -0,0 +1,10 @@
+def MyFunction(input_num):
+
+    my_list = [0,1,2,3]
+    if 1 in my_list: ind = 1
+    else:
+      ind = 0
+    qq = []
+    for i in my_list [ind:]:
+        qq.append(input_num/i)
+    return qq
